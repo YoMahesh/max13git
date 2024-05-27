c719e85437abe6ac266686cd1d0df52760061377
MaheshSulegai <156065935+YoMahesh@users.noreply.github.com>
2024-05-27 15:14:11 +0530

    Update mul.java

--- mul.java
+++ mul.java
@@ -1,7 +1,7 @@
 public class multiplication
 {
     public static void main(String[] args) {
-        int a = 4, b = a+5;
-        System.out.println(a*b);
+        int a = 4, b = 5, c=4;
+        System.out.println(a*b*c);
     }
 }
